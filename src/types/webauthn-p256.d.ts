a06d0355396ece67ad7400c77f40e1c8c523c3ff
0age <37939117+0age@users.noreply.github.com>
2024-12-03 14:09:17 -0800

    clear warning

--- src/types/webauthn-p256.d.ts
+++ src/types/webauthn-p256.d.ts
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "webauthn-p256" {
   type Record<K extends string | number | symbol, V> = { [P in K]: V };
   export type fallback = Record<string, unknown>;
 }
+
